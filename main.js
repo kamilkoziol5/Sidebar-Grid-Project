918aef23206b5d2e78e3dfd2b800443947b5c289
Kamil <kamilkoziol529@gmail.com>
2025-03-17 10:50:02 +0100

    add aria label and tittle to toggle button

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const sidebar = document.querySelector(".sidebar");
 	const toggleBtn = document.getElementById("toogle-btn");
-	const toggleIcon = toggleBtn.querySelector("i"); // Pobieramy ikonę wewnątrz przycisku
-	
+	const toggleIcon = toggleBtn.querySelector("i");
+
 	toggleBtn.addEventListener("click", () => {
 		sidebar.classList.toggle("open");
 		toggleIcon.classList.toggle("rotate");
+
+		// Aktualizacja `title` i `aria-label`
+		if (sidebar.classList.contains("open")) {
+			toggleBtn.setAttribute("title", "Close Sidebar");
+			toggleBtn.setAttribute("aria-label", "Close Sidebar");
+		} else {
+			toggleBtn.setAttribute("title", "Open Sidebar");
+			toggleBtn.setAttribute("aria-label", "Open Sidebar");
+		}
 	});
 });
