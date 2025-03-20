574dc5b83a1f83782fb7fc0d30cb61893f208440
Kamil <kamilkoziol529@gmail.com>
2025-03-20 11:18:12 +0100

    add tolltips for closed sidebar

--- main.js
+++ main.js
@@ -6,15 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
 	const mobileMenu = document.querySelector(".mobile-menu");
 	const closeIcon = document.querySelector(".close-toggle-btn");
 
+	// Toggle Sidebar
 	toggleBtn.addEventListener("click", () => {
 		sidebar.classList.toggle("open");
 		toggleBtnIcon.classList.toggle("rotate");
+
+		// Ukrywanie tooltipów, gdy sidebar jest otwarty
+		if (sidebar.classList.contains("open")) {
+			document
+				.querySelectorAll(".tooltip")
+				.forEach(tooltip => (tooltip.style.display = "none"));
+		} else {
+			document
+				.querySelectorAll(".tooltip")
+				.forEach(tooltip => (tooltip.style.display = "block"));
+		}
 	});
 
+	// Toggle Mobile Menu
 	hamburgerMenu.addEventListener("click", () => {
 		mobileMenu.classList.toggle("show");
 	});
 
+	// Close Mobile Menu
 	closeIcon.addEventListener("click", () => {
 		mobileMenu.classList.toggle("show");
 	});
